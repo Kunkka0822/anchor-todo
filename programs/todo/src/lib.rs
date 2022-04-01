6c69a9c6e7fed8a66f070eb9b1daf9178be8ef47
Kunkka0822 <101172481+Kunkka0822@users.noreply.github.com>
2022-04-01 11:18:15 -0700

    wip

--- programs/todo/src/lib.rs
+++ programs/todo/src/lib.rs
@@ -2,14 +2,62 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-#[program]
-pub mod todo {
-    use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+#[account]
+pub struct TodoList {
+    pub list_owner: Pubkey,
+    pub bump: u8,
+    pub capacity: u16,
+    pub name: String,
+    pub lines: Vec<Pubkey>,
+}
+
+impl TodoList {
+    fn space(name: &str, capacity: u16) -> usize {
+        // discriminator + owner pubkey + bump + capacity
+        8 + 32 + 1 + 2 + 
+        // name string
+        4 + name.len() +
+        // vec of item pubkeys
+        4 + (capacity as usize) * std::mem::size_of::<Pubkey>()
     }
 }
 
+
+fn name_seed(name: &str) -> &[u8] {
+    let b = name.as_bytes();
+    if b.len() > 32 { &b[0..32] } else { b }
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String, capacity: u16, list_bump: u8)]
+pub struct NewList<'info> {
+    #[account(init, payer=user,
+        space=TodoList::space(&name, capacity),
+        seeds=[
+            b"todolist",
+            user.to_account_info().key_as_ref(),
+            name_seed(&name)
+        ],
+        bump=list_bump)]
+    pub list: Account<'info, TodoList>,
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[program]
+pub mod todo {
+    pub fn new_list(
+        ctx: Context<NewList>,
+        name: String,
+        capacity: u16,
+        account_bump: u8,
+    ) -> ProgramResult {
+        let list = &mut ctx.accounts.list;
+        list.list_owner = *ctx.accounts.user.to_account_infos().key;
+        list.bump = account_bump;
+        list.name = name;
+        list.capacity = capacity;
+    }
+}
--- tests/todo.ts
+++ tests/todo.ts
@@ -1,16 +1,78 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Todo } from "../target/types/todo";
+const anchor = require('@project-serum/anchor');
+const BN = require('bn.js');
+const expect = require('chai').expect;
+const { SystemProgram, LAMPORTS_PER_SOL } = anchor.web3;
 
-describe("todo", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+const provider = anchor.Provider.env();
+anchor.setProvider(provider);
+const mainProgram = anchor.workspace.Todo;
 
-  const program = anchor.workspace.Todo as Program<Todo>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+
+async function getAccountBalance(pubkey) {
+  let account = await provider.connection.getAccountInfo(pubkey);
+  return account?.lamports ?? 0;
+}
+
+function expectBalance(actual, expected, message, slack = 20000) {
+  expect(actual, message).within(expected - slack, expected + slack);
+}
+
+async function createUser(airdropBalance) {
+  airdropBalance = airdropBalance ?? 10 * LAMPORTS_PER_SOL;
+  let user = anchor.web3.Keypair.generate();
+  let sig = await provider.connection.requestAirdrop(user.publicKey, airdropBalance);
+  await provider.connection.confirmTransaction(sig);
+
+  let wallet = new anchor.Wallet(user);
+  let userProvider = new anchor.Provider(provider.connection, wallet, provider.opts);
+
+  return {
+    key: user,
+    wallet,
+    provider: userProvider,
+  };
+}
+
+function createUsers(numUsers) {
+  let promises = [];
+  for (let i = 0; i < numUsers; i++) {
+    promises.push(createUser());
+  }
+
+  return Promise.all(promises);
+}
+
+function programForUser(user) {
+  return new anchor.Program(mainProgram.idl, mainProgram.ProgramId, user.provider);
+}
+
+async function createList(owner, name, capacity = 16) {
+  const [listAccount, bump] = await anchor.web3.PublicKey.findProgramAddress(
+    ['todolist', owner.key.publicKey.toBytes(), name.slice(0, 32)],
+    mainProgram.ProgramId
+  );
+
+  let program = programForUser(owner);
+  await program.rpc.newList(name, capacity, bump, {
+    accounts: {
+      list: listAccount,
+      user: owner.key.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
   });
-});
+
+  let list = await program.account.todoList.fetch(listAccount);
+  return { publicKey: listAccount, data: list };
+}
+
+describe('new list', () => {
+  it('creates a list', async () => {
+    const owner = await createUser();
+    let list = await createList(owner, 'A list');
+
+    expect(list.data.listOwner.toString(), 'List owner is set').equals(owner.key.publicKey.toString());
+    expect(list.data.name, 'List name is set').equals('A list');
+    expect(list.data.lines.length, 'List has no items').equals(0);
+  })
+})

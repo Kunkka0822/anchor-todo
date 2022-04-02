e2147014cd7bb3a3cefb2a68f09c3eded571ef3d
Kunkka0822 <101172481+Kunkka0822@users.noreply.github.com>
2022-04-02 06:46:55 -0700

    wip

--- programs/todo/src/lib.rs
+++ programs/todo/src/lib.rs
@@ -20,6 +20,47 @@ pub mod todo {
         list.capacity = capacity;
         Ok(())
     }
+
+    pub fn add(
+        ctx: Context<NewList>,
+        _list_name: String,
+        item_name: String,
+        bounty: u64,
+    ) -> ProgramResult {
+        let user = &ctx.accounts.user;
+        let list = &mut ctx.accounts.list;
+        let item = &mut ctx.accounts.item;
+
+        if list.lines.len() >= list.capacity as usize {
+            return Err(TodoListError::ListFull.into());
+        }
+
+        list.lines.push(*item.to_account_info().key);
+        item.name = item_name;
+        item.creator = *user.to_account_infos().key;
+
+        let account_lamports = **item.to_account_info.account_lamports.borrow();
+        let transfer_amount = bounty
+            .checked_sub(account_lamports)
+            .ok_or(TodoListError::BountyTooSmall)?;
+
+        if transfer_amount > 0 {
+            invoke(
+                &transfer(
+                    user.to_account_info().key,
+                    item.to_account_info().key,
+                    transfer_amount,
+                ),
+                &[
+                    user.to_account_info(),
+                    item.to_account_info(),
+                    ctx.accounts.system_program.to_account_info(),
+                ],
+            )?;
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -39,6 +80,25 @@ pub struct NewList<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(list_name: String, item_name: String, bounty: u64)]
+pub struct Add<'info> {
+    #[account(
+        mut,
+        has_one=list_owner @ TodoListError::WrongListOwner,
+        seeds=[
+            b"todolist",
+            list_owner.to_account_info.key().as_ref(),
+            name_seed(&list_name)
+        ], bump)]
+    pub list: Account<'info, TodoList>,
+    pub list_owner: AccountInfo<'info>,
+
+    #[account(init, payer=user, space=ListItem::space(&item_name))]
+    pub item: Account<'info, ListItem>,
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 pub struct TodoList {
@@ -59,6 +119,22 @@ impl TodoList {
         4 + (capacity as usize) * std::mem::size_of::<Pubkey>()
     }
 }
+
+#[account]
+pub struct ListItem {
+    pub creator: Pubkey,
+    pub creator_finished: bool,
+    pub list_owner_finished: bool,
+    pub name: String,
+}
+
+impl ListItem {
+    fn space(name: &str) -> usize {
+        //discriminator + creator pubkey + 2 bools + name string
+        8 + 32 + 2 + 4 + name.len()
+    }
+}
+
 
 
 fn name_seed(name: &str) -> &[u8] {
--- tests/todo.ts
+++ tests/todo.ts
@@ -4,6 +4,7 @@ import {
 } from '@solana/web3.js';
 import { Todo } from "../target/types/todo";
 
+const BN = require('bn.js');
 const expect = require('chai').expect;
 const { SystemProgram, LAMPORTS_PER_SOL } = anchor.web3;
 
@@ -76,6 +77,40 @@ async function createList(owner: {
   return { publicKey: listAccount, data: list };
 }
 
+async function addItem({ list, user, name, bounty }) {
+  const itemAccount = anchor.web3.Keypair.generate();
+  let program = programForUser(user);
+  await program.rpc.add(list.data.name, name, new BN(bounty), {
+    accounts: {
+      list: list.publicKey,
+      listOwner: list.data.listOwner,
+      item: itemAccount.publicKey,
+      user: user.key.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [
+      user.key,
+      itemAccount,
+    ],
+  });
+
+  let [listData, itemData] = await Promise.all([
+    program.account.todoList.fetch(list.publicKey),
+    program.account.listItem.fetch(itemAccount.publicKey),
+  ]);
+
+  return {
+    list: {
+      publicKey: list.publicKey,
+      data: listData,
+    },
+    item: {
+      publicKey: itemAccount.publicKey,
+      data: itemData
+    },
+  };
+}
+
 describe('new list', () => {
   it('creates a list', async () => {
     const owner = await createUser();
@@ -84,5 +119,78 @@ describe('new list', () => {
     expect(list.data.listOwner.toString(), 'List owner is set').equals(owner.key.publicKey.toString());
     expect(list.data.name, 'List name is set').equals('A list');
     expect(list.data.lines.length, 'List has no items').equals(0);
+  })
+})
+
+describe('add', () => {
+  it('Anyone can add an item to a list', async () => {
+    const [owner, adder] = await createUsers(2);
+
+    const adderStartingBalance = await getAccountBalance(adder.key.publicKey);
+    const list = await createList(owner, 'list');
+    const result = await addItem({
+      list,
+      user: adder,
+      name: 'Do something',
+      bounty: 1 * LAMPORTS_PER_SOL,
+    });
+
+    expect(result.list.data.lines, 'Item is added').deep.equals([result.item.publicKey]);
+    expect(result.item.data.creator.toString(), 'Item marked with creator').equals(adder.key.publicKey.toString());
+    expect(result.item.data.creatorFinished, 'creator_finished is false').equals(false);
+    expect(result.item.data.listOwnerFinished, 'list_owner_finished is false').equals(false);
+    expect(result.item.data.name, 'Name is set').equals('Do something');
+    expect(await getAccountBalance(result.item.publicKey), 'List account balace').equals(1 * LAMPORTS_PER_SOL);
+
+    let adderNewBalance = await getAccountBalance(adder.key.publicKey);
+    expectBalance(
+      adderStartingBalance - adderNewBalance,
+      LAMPORTS_PER_SOL,
+      'Number of lamports removed from adder is equal to bounty'
+    );
+
+    const again = await addItem({
+      list,
+      user: adder,
+      name: 'Another item',
+      bounty: 1 * LAMPORTS_PER_SOL,
+    });
+    expect(again.list.data.lines, 'Item is added').deep.equals([result.item.publicKey, again.item.publicKey]);
+  })
+
+  it('fails if the list is full', async () => {
+    const MAX_LIST_SIZE = 4;
+    const owner = await createUser();
+    const list = await createList(owner, 'list', MAX_LIST_SIZE);
+
+    await Promise.all(
+      new Array(MAX_LIST_SIZE).fill(0).map((_, i) => {
+        return addItem({
+          list,
+          user: owner,
+          name: `Item ${i}`,
+          bounty: 1 * LAMPORTS_PER_SOL,
+        });
+      })
+    );
+
+    const adderStartingBalance = await getAccountBalance(owner.key.publicKey);
+
+    try {
+      let addResult = await addItem({
+        list,
+        user: owner,
+        name: 'Full item',
+        bounty: 1 * LAMPORTS_PER_SOL,
+      });
+
+      console.dir(addResult, {depth: null});
+      expect.fail('Adding to full list should have failed');
+    } catch (e) {
+      expect(e.toString()).contains('This list is full');
+    }
+
+    let adderNewBalance = await getAccountBalance(owner.key.publicKey);
+    expect(adderStartingBalance, 'Adder balance is unchaged.').equals(adderNewBalance);
   })
 })
